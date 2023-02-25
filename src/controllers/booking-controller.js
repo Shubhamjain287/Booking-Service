dda99d7326b7c967a7be2564dc485eb347a5b922
Shubhamjain287 <Shubhamjainpvt28@gmail.com>
2023-02-25 14:51:50 +0530

    RabbitMQ - Message Queue Setup Done

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -1,28 +1,44 @@
 const { BookingService } =require("../services/index");
 const { StatusCodes } = require("http-status-codes");
+const { createChannel , publushMessage } = require("../utils/messageQueues");
+const { REMINDER_BINDING_KEY } = require("../config/serverConfig");
 
 const bookingService = new BookingService();
 
-const create = async (req,res) => {
-    try {
-        const response = await bookingService.createBooking(req.body);
-        return res.status(StatusCodes.OK).json({
-            data: response,
-            success: true,
-            message: `Successfully Completed Booking`,
-            err: {}
-        });
-    } catch (error) {
-        console.log(error);
-        return res.status(error.statusCodes).json({
-            data: {},
-            success: false,
-            message: error.message,
-            err: error.explanation
-        });
+class BookingController{
+
+    constructor(){
+        
+    }
+
+    async sendMessageToQueue(req,res){
+        const channel = await createChannel();
+        const data = { message : "Success"};
+        publushMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(data));
+        return res.status(200).json({
+            message: "Successfully published the event"
+        })
+    }
+
+    async create (req,res) {
+        try {
+            const response = await bookingService.createBooking(req.body);
+            return res.status(StatusCodes.OK).json({
+                data: response,
+                success: true,
+                message: `Successfully Completed Booking`,
+                err: {}
+            });
+        } catch (error) {
+            console.log(error);
+            return res.status(error.statusCodes).json({
+                data: {},
+                success: false,
+                message: error.message,
+                err: error.explanation
+            });
+        }
     }
 }
 
-module.exports = {
-    create
-}
+module.exports = BookingController;
---
+++ src/utils/messageQueues.js
@@ -0,0 +1,50 @@
+const amqplib = require("amqplib");
+const { EXCHANGE_NAME, MESSAGE_BROKER_URL } = require("../config/serverConfig");
+
+const createChannel = async () => {
+    try {
+
+        const connection = await amqplib.connect(MESSAGE_BROKER_URL);
+        const channel = await connection.createChannel();
+
+        await channel.assertExchange(EXCHANGE_NAME, 'direct', false);
+
+        return channel;
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const subscribeMessage = async (channel, service, binding_key) => {
+
+    try {
+        const applicationQueue = await channel.assertQueue('EMAIL_QUEUE');
+
+    channel.bindQueue(applicationQueue.queue, EXCHANGE_NAME, binding_key);
+
+    channel.consume(applicationQueue.queue, (msg) => {
+        console.log('Data Receiverd');
+        console.log(msg.content.toString());
+        channel.ack(msg);
+    });
+    } catch (error) {
+       throw error; 
+    }
+
+}
+
+const publushMessage = async (channel, binding_key, message) => {
+    try {
+        await channel.assertQueue('EMAIL_QUEUE')
+        await channel.publish(EXCHANGE_NAME, binding_key, Buffer.from(message));
+    } catch (error) {
+        throw error;
+    }
+}
+
+module.exports = {
+    createChannel,
+    subscribeMessage,
+    publushMessage
+}
